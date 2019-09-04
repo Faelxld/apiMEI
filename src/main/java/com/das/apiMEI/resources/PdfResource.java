316ff65b388ad463751ac66b88b188aaf39fe9c2
rafael <rafaelxld@hotmail.com>
2019-09-04 09:48:54 -0300

    crawler correcao

--- src/main/java/com/das/apiMEI/resources/PdfResource.java
+++ src/main/java/com/das/apiMEI/resources/PdfResource.java
@@ -32,7 +32,7 @@ public class PdfResource {
         List<Pdf> filtro =  new ArrayList<>();
 
         for (Pdf pdf:pdfs) {
-            if(pdf.getCnpj().equals(cnpj) && pdf.isPartial()){
+            if(pdf.getCnpj().equals(cnpj) && pdf.isLido()){
                 filtro.add(pdf);
             }
         }
